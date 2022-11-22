a9f63dc8dad8f890e5fae5a64dcfb4e6a3a70582
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-11-23 06:33:33 +0700

    Progate Study PHP Version 4

---
+++ README.md
@@ -0,0 +1,3 @@
+# Progate Study PHP
+
+<h2>Progate Study PHP Version 4</h2>
---
+++ page11/review.php
@@ -0,0 +1,38 @@
+<?php
+class Review {
+  private $menuName;
+  // Deklarasikan property private $userName
+  private $userName;
+  
+  private $body;
+
+  // Tambahkan $userName ke parameter kedua
+  public function __construct($menuName, $userName, $body) {
+    $this->menuName = $menuName;
+    // Tetapkan $userName ke property userName
+    $this->userName = $userName;
+    
+    $this->body = $body;
+  }
+
+  public function getMenuName() {
+    return $this->menuName;
+  }
+
+  public function getBody() {
+    return $this->body;
+  }
+  
+  // Definisikan method getUser
+  public function getUser($users) {
+  //   return $this->users;
+  // }
+  foreach ($users as $user) {
+      if ($user->getName() == $this->userName) {
+        return $user;
+      }
+  }}
+  
+}
+
+?>
